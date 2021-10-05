7767a4ce022d1e7b341624c3cd95526c7ee07968
ryanhyunlee <ryanjhlee87@outlook.com>
2021-10-05 11:56:01 +0900

    Add search

--- src/App.js
+++ src/App.js
@@ -1,30 +1,49 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import axios from "axios";
 
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
+import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
 
 const App = () => {
   const [users, setUsers] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState(null);
 
-  const fetchUsers = async () => {
+  // Search Github users
+  const searchUsers = async (text) => {
     setLoading(true);
-    const githubUsers = await axios.get("https://api.github.com/users");
-    setUsers(githubUsers.data);
+    const searchResult = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+    );
+    setUsers(searchResult.data.items);
     setLoading(false);
   };
 
-  useEffect(() => {
-    fetchUsers();
-    console.log(users);
-  }, []);
+  const clearUsers = () => {
+    setLoading(true);
+    setUsers([]);
+    setLoading(false);
+  };
+
+  const showAlert = (msg, type) => {
+    setAlert({ msg, type });
+    setTimeout(() => setAlert(null), 3000);
+  };
 
   return (
     <div>
       <Navbar />
       <div className='container'>
+        {alert !== {} ? <Alert alert={alert} /> : {}}
+        <Search
+          searchUsers={searchUsers}
+          clearUsers={clearUsers}
+          showClear={users.length > 0 ? true : false}
+          showAlert={showAlert}
+        ></Search>
         <Users users={users} loading={loading} />
       </div>
     </div>
---
+++ src/components/users/Search.js
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+
+const Search = ({ searchUsers, clearUsers, showClear, showAlert }) => {
+  const [text, setText] = useState("");
+
+  const onTextChange = (e) => {
+    setText(e.target.value);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (text === "") {
+      showAlert("Please enter something...", "light");
+    } else {
+      searchUsers(text);
+      setText("");
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit} className='form'>
+        <input
+          onChange={onTextChange}
+          type='text'
+          className='text'
+          placeholder='Search user...'
+          value={text}
+        />
+        <input
+          type='submit'
+          value='Search'
+          className='btn btn-dark btn-block'
+        />
+      </form>
+      {showClear && (
+        <button onClick={clearUsers} className='btn btn-light btn-block'>
+          Clear
+        </button>
+      )}
+    </div>
+  );
+};
+
+Search.propTypes = {
+  searchUsers: PropTypes.func.isRequired,
+  clearUsers: PropTypes.func.isRequired,
+  showClear: PropTypes.bool.isRequired,
+  showAlert: PropTypes.func.isRequired,
+};
+
+export default Search;
